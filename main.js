8cff04b5389378261eefa1db6f36c20249cc311f
Nita <cburtoncareers@yahoo.com>
2020-07-17 18:22:36 -0400

    added child classes

--- main.js
+++ main.js
@@ -36,6 +36,56 @@ console.log(angelica)
 angelica.eat()
 console.log(angelica)
 
+//Create the Doctor Class that extends from the Traveler Class
+
+class Doctor extends Traveler {
+    constructor (name, food, isHealthy) {
+        super (name, food)
+        this.isHealthy = isHealthy
+    }
+
+    heal(traveler) {
+        traveler.isHealthy = true
+    }
+}
+
+//Create the Hunter Class that extends from the Traveler Class
+
+class Hunter extends Traveler {
+    constructor (name, food, isHealthy) {
+        super (name, food)
+        this.food = 2
+        this.isHealthy = isHealthy
+    }
+
+    hunt() {
+        this.food += 5
+    }
+
+    eat() {
+        
+        if (this.food < 2) {
+        
+            this.isHealthy = false
+            this.food = 0
+
+        }
+        else {
+            this.food -= 2
+        }
+    }
+
+    giveFood(traveler, numOfFoodUnits) {
+
+        if (this.food >= numOfFoodUnits ) {
+            traveler.food += numOfFoodUnits
+            this.food -= numOfFoodUnits
+        }
+        
+        
+    }
+}
+
 //Create the Wagon Class
 
 class Wagon {
